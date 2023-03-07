b126432194e6ceb9e268789cb14e719e2be7f7c4
Patrick <patrickmahinay031@gmail.com>
2023-03-07 14:06:29 +0800

    fix route issue

---
+++ src/Routes/routes.js
@@ -0,0 +1,23 @@
+import Login from "../Components/Login/Login";
+import Medicine from "../Components/Medicine/Medicine";
+// import Patient from "../Components/Patient/Patient";
+
+export const loginRoutes = [
+  {
+    component: <Login />,
+    path: "",
+  },
+];
+
+export const adminRoutes = [
+//   {
+//     component: <Patient />,
+//     title: "Patient",
+//     path: "patient",
+//   },
+  {
+    component: <Medicine />,
+    title: "Medicine",
+    path: "medicine",
+  },
+];
